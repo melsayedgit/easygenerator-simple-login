0a04d899bf77946bc58ed255286499875812b2d5
Mohamed Elsayed <odsh97@gmail.com>
2025-04-25 23:59:28 +0300

    feat: signup and signin method

--- apps/server/src/auth/auth.controller.ts
+++ apps/server/src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { SigninDTO } from './dto/signin.dto';
+import { SignupDTO } from './dto/signup.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  @Post('signup')
+  signup(@Body() SignupDTO: { email: string; password: string }) {
+    return 'signup';
+  }
+  @Post('signin')
+  signin(@Body() SigninDTO: { email: string; password: string }) {
+    return 'signin';
+  }
+}

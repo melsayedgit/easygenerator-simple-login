a973a20f06e4d4a935fce4b42ec05b031c6069d4
Mohamed Elsayed <odsh97@gmail.com>
2025-04-25 19:15:38 +0300

    feat:auth jwt and local strategies

---
+++ apps/server/src/auth/strategies/local.strategy.ts
@@ -0,0 +1,23 @@
+import { Strategy, IStrategyOptions } from 'passport-local';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { AuthService } from '../auth.service';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly authService: AuthService) {
+    const options: IStrategyOptions = {
+      usernameField: 'email',
+    };
+
+    super(options);
+  }
+
+  async validate(email: string, password: string) {
+    const user = await this.authService.validateUser(email, password);
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+    return user;
+  }
+}

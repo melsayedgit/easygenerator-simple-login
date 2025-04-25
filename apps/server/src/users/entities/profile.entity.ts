c9b1efb0e1afb7024668ac14eb39966264661f04
Mohamed Elsayed <odsh97@gmail.com>
2025-04-25 21:16:41 +0300

    feat: simple profile

--- apps/server/src/users/entities/profile.entity.ts
+++ apps/server/src/users/entities/profile.entity.ts
@@ -5,18 +5,6 @@ export type ProfileDocument = HydratedDocument<Profile>;
 @Schema({ _id: false })
 export class Profile {
   @Prop({
-    maxlength: 20,
-    validate: {
-      validator: (username: string) => /^[\w.-]+$/.test(username),
-      message: (props: { value: string }) =>
-        `username:${props.value} contains invalid characters!,
-         you can only use only allows safe, unencoded URL elements,
-         alphabet,digits,"-","_","." and no whitespaces`,
-    },
-  })
-  username?: string;
-
-  @Prop({
     maxlength: 30,
   })
   name?: string;
@@ -26,8 +14,3 @@ export class Profile {
 }
 
 export const ProfileSchema = SchemaFactory.createForClass(Profile);
-
-ProfileSchema.index(
-  { username: 1 },
-  { unique: true, sparse: true, collation: { locale: 'en', strength: 2 } },
-);

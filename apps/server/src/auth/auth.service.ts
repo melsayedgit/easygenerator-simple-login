b14621d50d2b983c66fa8d0a2a73146906a27b41
Mohamed Elsayed <odsh97@gmail.com>
2025-04-25 19:16:17 +0300

    feat: auth validateUser service

--- apps/server/src/auth/auth.service.ts
+++ apps/server/src/auth/auth.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from 'src/users/entities/user.entity';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectModel(User.name)
+    private userModel: Model<User>,
+  ) {}
+
+  async validateUser(email: string, password: string): Promise<User | null> {
+    const user = await this.userModel.findOne({ email });
+    if (user && (await user.comparePassword(password))) {
+      return user;
+    }
+    return null;
+  }
+}

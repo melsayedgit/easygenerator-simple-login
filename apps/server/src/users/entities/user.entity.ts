9e20b8c014360d6c56b2666cfdc7b628cedab274
Mohamed Elsayed <odsh97@gmail.com>
2025-04-25 14:51:06 +0300

    feat: adding password comparing, hashing and salting to mongoos schema

--- apps/server/src/users/entities/user.entity.ts
+++ apps/server/src/users/entities/user.entity.ts
@@ -1,7 +1,7 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument, Types } from 'mongoose';
 import { Profile, ProfileSchema } from './profile.entity';
-
+import * as bcrypt from 'bcrypt';
 export type UserDocument = HydratedDocument<Omit<User, 'profile'>> & {
   profile?: Types.Subdocument<unknown, unknown, Profile> & Profile;
 };
@@ -16,5 +16,26 @@ export class User {
 
   @Prop({ type: ProfileSchema })
   profile?: Profile;
+
+  comparePassword = async function (
+    this: User,
+    password: string,
+  ): Promise<boolean> {
+    return await bcrypt.compare(password, this.password);
+  };
 }
 export const UserSchema = SchemaFactory.createForClass(User);
+
+UserSchema.methods.comparePassword = async function (
+  this: UserDocument,
+  password: string,
+): Promise<boolean> {
+  return await bcrypt.compare(password, this.password);
+};
+
+UserSchema.pre<UserDocument>('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});

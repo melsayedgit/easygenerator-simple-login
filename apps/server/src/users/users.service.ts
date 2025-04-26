364398a6f30f70e97113ec026854c6972ed2b367
Mohamed Elsayed <odsh97@gmail.com>
2025-04-26 13:26:17 +0300

    feat: create user service

--- apps/server/src/users/users.service.ts
+++ apps/server/src/users/users.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from './entities/user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectModel(User.name)
+    private readonly userModel: Model<
+      User,
+      Record<string, never>,
+      Record<string, never>,
+      Record<string, never>,
+      UserDocument
+    >,
+  ) {}
+  async findOne(email: string): Promise<any> {
+    const user = await this.userModel.findOne({ email });
+    if (!user) {
+      throw new ConflictException(`User with email ${email} already exists`);
+    }
+    return user;
+  }
+}

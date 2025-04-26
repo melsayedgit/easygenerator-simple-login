fb1b4e694fa4d983a31e8d539d2b7954d7db5603
Mohamed Elsayed <odsh97@gmail.com>
2025-04-27 01:58:01 +0300

    feat: implmenting signup and signin controller

--- apps/server/src/auth/auth.controller.ts
+++ apps/server/src/auth/auth.controller.ts
@@ -1,15 +1,28 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { SigninDTO } from './dto/signin.dto';
 import { SignupDTO } from './dto/signup.dto';
+import { UsersService } from '../users/users.service';
+import { Profile } from 'src/users/entities/profile.entity';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly authService: AuthService,
+  ) {}
+
   @Post('signup')
-  signup(@Body() SignupDTO: { email: string; password: string }) {
-    return 'signup';
+  signup(@Body() signupDTO: SignupDTO) {
+    return this.usersService.create(
+      signupDTO.email,
+      signupDTO.password,
+      signupDTO.profile as Profile,
+    );
   }
+
   @Post('signin')
-  signin(@Body() SigninDTO: { email: string; password: string }) {
-    return 'signin';
+  signin(@Body() sigingDTO: SigninDTO) {
+    return this.authService.validateUser(sigingDTO.email, sigingDTO.password);
   }
 }
